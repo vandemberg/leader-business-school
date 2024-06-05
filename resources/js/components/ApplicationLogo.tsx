ad01323cd577e1166cc6e12b72695d290e04c0f0
vandemberg lima <vandemberg.silva.lima@gmail.com>
2024-06-04 22:57:42 -0300

    Hotfix: Logo Application navbar width auto

--- resources/js/components/ApplicationLogo.tsx
+++ resources/js/components/ApplicationLogo.tsx
@@ -3,9 +3,8 @@ import logo from '@/assets/images/lider-sandbox-1.png';
 
 export default function ApplicationLogo(props: StyleHTMLAttributes<HTMLImageElement>) {
     const finalHeight = props.style?.height || 100;
-    const finalWidth = props.style?.width || 100;
 
     return (
-        <img src={logo} style={{ height: finalHeight, width: finalWidth, ...props }} />
+        <img src={logo} style={{ height: finalHeight, width: "auto", ...props }} />
     );
 }
